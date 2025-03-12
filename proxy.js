ad70d7cef49779893a0946f6f19e94ee08ac4540
CWD273 <itscwd273@gmail.com>
2025-03-12 18:10:48 -0500

    Add files via upload

---
+++ proxy.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createProxyServer({});
+const server = http.createServer((req, res) => {
+    proxy.web(req, res, { target: 'http://fl2.moveonjoy.com/CARTOON_NETWORK/index.m3u8?checkedby:alliptvlinks.com
+' });
+});
+
+server.listen(8080, () => {
+    console.log('Proxy server is running on http://localhost:8080');
+});
