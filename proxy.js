85a939fe913f3b9dc2a643d7335842f1fc69f70f
CWD273 <itscwd273@gmail.com>
2025-03-12 18:29:45 -0500

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,14 +1,26 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
+// Create a proxy server
+const proxy = httpProxy.createProxyServer({});
+
+// Create an HTTP server and handle incoming requests
 const server = http.createServer((req, res) => {
+    // Add CORS headers to allow cross-origin requests
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    proxy.web(req, res, { target: 'http://fl2.moveonjoy.com/CARTOON_NETWORK/index.m3u8?checkedby:alliptvlinks.com' });
+
+    // Proxy the request to the target URL
+    proxy.web(req, res, {
+        target: 'http://fl2.moveonjoy.com/CARTOON_NETWORK/index.m3u8?checkedby:alliptvlinks.com'
+    });
 });
 
-const PORT = process.env.PORT || 8080; // Use Render's PORT environment variable
+// Use the PORT environment variable provided by Render or default to 8080
+const PORT = process.env.PORT || 8080;
+
+// Start the server
 server.listen(PORT, () => {
     console.log(`Proxy server is running on http://localhost:${PORT}`);
 });
