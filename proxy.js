05f5404de3b9efe6d431c882fb6867a6d3498309
CWD273 <itscwd273@gmail.com>
2025-03-12 18:27:44 -0500

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-const proxy = httpProxy.createProxyServer({});
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     proxy.web(req, res, { target: 'http://fl2.moveonjoy.com/CARTOON_NETWORK/index.m3u8?checkedby:alliptvlinks.com' });
 });
 
